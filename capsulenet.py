f5064315752373c137fe0d9fc794b3cefe81482e
SudeepSarkar <sarkar@usf.edu>
2019-03-15 18:27:52 -0400

    Update capsulenet.py

--- capsulenet.py
+++ capsulenet.py
@@ -203,6 +203,17 @@ def load_mnist():
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
 
+def load_fashion_mnist():
+    # the data, shuffled and split between train and test sets
+    from keras.datasets import fashion_mnist
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+    x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    y_train = to_categorical(y_train.astype('float32'))
+    y_test = to_categorical(y_test.astype('float32'))
+    return (x_train, y_train), (x_test, y_test)
+
 
 if __name__ == "__main__":
     import os
@@ -240,7 +251,7 @@ if __name__ == "__main__":
         os.makedirs(args.save_dir)
 
     # load data
-    (x_train, y_train), (x_test, y_test) = load_mnist()
+    (x_train, y_train), (x_test, y_test) = load_fashion_mnist()
 
     # define model
     model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
