2d15d741d95d128367792b0d03d7a9bc39b389a9
SudeepSarkar <sarkar@usf.edu>
2019-03-15 21:02:18 -0400

    Update capsulenet.py

--- capsulenet.py
+++ capsulenet.py
@@ -170,26 +170,29 @@ def test(model, data, args):
 def manipulate_latent(model, data, args):
     print('-'*30 + 'Begin: manipulate' + '-'*30)
     x_test, y_test = data
-    index = np.argmax(y_test, 1) == args.digit
-    number = np.random.randint(low=0, high=sum(index) - 1)
-    x, y = x_test[index][number], y_test[index][number]
-    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
-    noise = np.zeros([1, 10, 16])
-    x_recons = []
-    for dim in range(16):
-        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
-            tmp = np.copy(noise)
-            tmp[:,:,dim] = r
-            x_recon = model.predict([x, y, tmp])
-            x_recons.append(x_recon)
+    for index in 1..np.argmax(y_test, 1)
+           #index = np.argmax(y_test, 1) == args.digit
+           number = np.random.randint(low=0, high=sum(index) - 1)
+           x, y = x_test[index][number], y_test[index][number]
+           x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
+           noise = np.zeros([1, 10, 16])
+           x_recons = []
+           for dim in range(16):
+               for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+                   tmp = np.copy(noise)
+                   tmp[:,:,dim] = r
+                   x_recon = model.predict([x, y, tmp])
+                   x_recons.append(x_recon)
 
-    x_recons = np.concatenate(x_recons)
+           x_recons = np.concatenate(x_recons)
 
-    img = combine_images(x_recons, height=16)
-    image = img*255
-    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
-    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
-    print('-' * 30 + 'End: manipulate' + '-' * 30)
+           img = combine_images(x_recons, height=16)
+           image = img*255
+           #Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
+           #print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
+           Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % index)
+           print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, index))
+           print('-' * 30 + 'End: manipulate' + '-' * 30)
 
 
 def load_mnist():
